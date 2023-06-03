fcf117741c8b45cfaab310e31efc077dc804047d
shubham <shubham.jadhav@ufaber.com>
2023-06-03 23:31:48 +0530

    initial commit done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+      <header>
         <p>
           this is our client side of the application
         </p>
